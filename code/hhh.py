d0400a958dbd9ae15077a5a2412e612a47c6a000
2530953905 <2530953905@qq.com>
2018-05-27 16:39:12 +0800

    11

--- code/hhh.py
+++ code/hhh.py
@@ -1,4 +1,4 @@
-import  pytest, allure
+import allure
 
 import pytest
 
@@ -12,5 +12,5 @@ class Test01:
         assert aaa == 1
     @allure.step(title="测试步骤002")
     @pytest.mark.parametrize("aaa",[4,2,3])
-    def test001(self,aaa):
+    def test002(self,aaa):
         assert aaa != 1
